22c1a9188e55fb477869274adaad8ca81a4f8cc1
asukas00 <jessere2025@gmail.com>
2025-10-24 06:49:24 -0700

    s

--- app.py
+++ app.py
@@ -4,3 +4,12 @@ movies = open("./movies.json", encoding="utf8")
 ## create variable "data" that represents the enitre movie list
 data = json.load(movies)
 
+""" for i in data:
+    print(i["title"]) """
+
+
+user_input = int(input("Are there any movies you’d like to watch that were released after a certain year?"))
+
+for i in data:
+    if i["year"] >= user_input:
+        print(i["title"])

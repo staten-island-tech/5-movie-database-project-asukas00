1711de80ee9891c74ba03ce586a5064ab70b38d2
asukas00 <jessere2025@gmail.com>
2025-10-28 07:17:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,14 +14,28 @@ data = json.load(movies)
     if i["year"] >= user_input:
         print(i["title"]) """
 
-user_input = int(input("Are there any movies you’d like to watch that were released during a certain year?"))
+
+""" user_input = int(input("Are there any movies you’d like to watch that were released during a certain year?"))
 
 for i in data:
     if i["year"] == user_input:
         print(i["title"])
-""" 
-user_input = int(input("Are there any movies you’d like to watch that were released before and after a certain year?"))
+ """
 
+""" user_input = int(input("What is the earliest year of movie you want?"))
+user_input1 = int(input("What is the lastest year of movie you want?"))
 for i in data:
-    if i["year"] >= user_input and i["year"] <= user_input:
-        print(i["title"] ) """
+    if i["year"] >= user_input and i["year"] <= user_input1:
+        print(i["title"]) """
+""" 
+x = input("what movie?")
+y = x.lower()
+for i in data:
+    if y in i["title"].lower():
+        print(i["title"])
+         """
+
+x = input("what genre?").lower()
+for i in data:
+    if x in i["genres"]:
+        print(i["title"])
